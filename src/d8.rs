f6965647e9e20ab1beab088ec5e079e67ccc3850
Nagy Máté <nmakaltar@gmail.com>
2022-12-12 14:28:49 +0100

    Solve day 8 part 2

--- src/d8.rs
+++ src/d8.rs
@@ -1,5 +1,54 @@
 use crate::common;
 
+const SIZE: usize = 99;
+type Forest = [[i64; SIZE]; SIZE];
+
+// too low: 611520
+fn get_tree_scenic_score(x: usize, y: usize, forest: Forest) -> i64 {
+    let to_left = (0..x).rev();
+    let to_right = (x + 1)..SIZE;
+    let to_top = (0..y).rev();
+    let to_bottom = (y + 1)..SIZE;
+
+    let our_height = forest[x][y];
+
+    let mut left_score = 0;
+    let mut right_score = 0;
+    let mut top_score = 0;
+    let mut bottom_score = 0;
+    for look_x in to_left {
+        left_score += 1;
+        if forest[look_x][y] >= our_height {
+            break;
+        }
+    }
+
+    for look_x in to_right {
+        right_score += 1;
+        if forest[look_x][y] >= our_height {
+            break;
+        }
+    }
+
+    for look_y in to_top {
+        top_score += 1;
+        if forest[x][look_y] >= our_height {
+            break;
+        }
+    }
+
+    for look_y in to_bottom {
+        bottom_score += 1;
+        if forest[x][look_y] >= our_height {
+            break;
+        }
+    }
+
+    //println!("lrtp: {left_score}, {right_score} {top_score} {bottom_score} ");
+
+    left_score * right_score * top_score * bottom_score
+}
+
 pub fn solve() {
     // more than 526
     // more than 1033
@@ -8,14 +57,13 @@ pub fn solve() {
 
     let all = common::read_file_to_string("input8.txt".to_string());
     let lines = all.lines();
-    const SIZE: usize = 99;
     let mut vis_matrix: [[bool; SIZE]; SIZE] = [[false; SIZE]; SIZE];
-    let mut height_matrix: [[i64; SIZE]; SIZE] = [[0; SIZE]; SIZE];
+    let mut forest: Forest = [[0; SIZE]; SIZE];
 
     for (line_index, line) in lines.enumerate() {
         for (char_index, char) in line.chars().enumerate() {
             let height = char as i64 - '0' as i64;
-            height_matrix[line_index][char_index] = height;
+            forest[line_index][char_index] = height;
         }
     }
 
@@ -29,26 +77,26 @@ pub fn solve() {
             let y_r = SIZE - y - 1;
 
             // top down
-            if height_matrix[x][y] > highest_x {
-                highest_x = height_matrix[x][y];
+            if forest[x][y] > highest_x {
+                highest_x = forest[x][y];
                 vis_matrix[x][y] = true;
             }
 
             // bottom up
-            if height_matrix[x][y_r] > highest_x_r {
-                highest_x_r = height_matrix[x][y_r];
+            if forest[x][y_r] > highest_x_r {
+                highest_x_r = forest[x][y_r];
                 vis_matrix[x][y_r] = true;
             }
 
             // left to right
-            if height_matrix[y][x] > highest_y {
-                highest_y = height_matrix[y][x];
+            if forest[y][x] > highest_y {
+                highest_y = forest[y][x];
                 vis_matrix[y][x] = true;
             }
 
             // right to left
-            if height_matrix[y_r][x] > highest_y_r {
-                highest_y_r = height_matrix[y_r][x];
+            if forest[y_r][x] > highest_y_r {
+                highest_y_r = forest[y_r][x];
                 vis_matrix[y_r][x] = true;
             }
         }
@@ -60,6 +108,21 @@ pub fn solve() {
             all_visible += 1;
         }
     }
-
     println!("Number of visible trees: {all_visible}");
+
+    let mut all_scores = Vec::new();
+    for x in 0..SIZE {
+        for y in 0..SIZE {
+            all_scores.push(get_tree_scenic_score(x, y, forest))
+        }
+    }
+    all_scores.sort();
+
+    let least_scenic = all_scores.first().unwrap();
+    let highest_scenic = all_scores.last().unwrap();
+
+    println!(
+        "Highest scenic:{highest_scenic}, {least_scenic}, num: {}",
+        all_scores.len()
+    );
 }
